37a8af176730ed405af88c6bdd46198c6af590f2
jchan3990 <jonchan3990@gmail.com>
2020-05-13 11:54:24 -0700

    Finished functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,30 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
+  var count = 0;
+  var first = 0;
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    if (count - first === 0) {
+      return undefined;
+    }
+
+    var result = storage[first];
+    delete storage[first];
+    first++;
+    return result;
   };
 
   someInstance.size = function() {
+    return count - first;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,29 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
+  var count = 0;
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+    if (count === 0) {
+      return undefined
+    }
+
+    count--;
+    var result = storage[count]
+    delete storage[count];
+
+    return result;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
