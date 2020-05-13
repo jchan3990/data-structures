d051e5422d540ea99b05c86e546d448da4e8dd74
jchan3990 <jonchan3990@gmail.com>
2020-05-13 12:29:06 -0700

    Revert "Finished functional-shared stack"
    This reverts commit 34c4ff77b90b9e0761faaa80dc2968ca06bf7616.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,35 +1,11 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var stack = {
-    storage: {},
-    count: 0
-  };
 
-  _.extend(stack, stackMethods);
-  return stack;
+  var storage = {};
+  var count = 0;
 };
 
-var stackMethods = {
-  push: function(value) {
-    this.storage[this.count] = value;
-    this.count++
-  },
-
-  pop: function() {
-    if (this.size() === 0) {
-      return undefined;
-    }
-
-    this.count--;
-    var result = this.storage[this.count];
-    delete this.storage[this.count];
-    return result;
-  },
-
-  size: function() {
-    return this.count;
-  }
-};
+var stackMethods = {};
 
 
