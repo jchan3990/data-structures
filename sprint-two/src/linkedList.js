c417761d5dd5cbd021c12ccb640328a8a8cacbed
jchan3990 <jonchan3990@gmail.com>
2020-05-14 10:56:09 -0700

    addToTail linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,23 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // create Node object
+    var newNode = Node(value);
+    // If list is empty, update both head and tail to new object
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+    // Else tail data/next updates, head next updates to tail node
+    // this is the current tail and we're setting the next point to our new node object
+    list.tail.next = newNode; // b
+    // so now we need to indicate what our new tail is
+    list.tail = newNode; // c - re-assigning list.tail
+    }
   };
 
   list.removeHead = function() {
+
   };
 
   list.contains = function(target) {
