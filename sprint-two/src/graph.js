b168c8ba06cf3ece273a4438eeb7f5d6d321dcf7
jchan3990 <jonchan3990@gmail.com>
2020-05-15 16:50:07 -0700

    Working on Graph remove and foreach

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,34 +2,65 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  // creates an object container
+  this.vertices = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  // add an object node to the graph with a property value of value
+  this.vertices[node] = {};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  // Check if key exists and return bool
+  return this.vertices.hasOwnProperty(node);
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+
+
+  // delete target node from graph
+  delete this.vertices[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  // Check the nodes to see if the they have the adjacent edges
+  if (this.vertices[fromNode].hasOwnProperty(toNode) && this.vertices[toNode].hasOwnProperty(fromNode)) {
+    return true;
+  } else {
+    return false;
+  }
+
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  // input: two nodes objects
+  // Add adjacent nodes as keys to their corresponding nodes with the value of true
+  this.vertices[fromNode][toNode] = true;
+  this.vertices[toNode][fromNode] = true;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  // delete appropriate edge property (connection) two both node objects
+  delete this.vertices[fromNode][toNode];
+  delete this.vertices[toNode][fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  // this is how we traverse the graph
+  var firstNode;
+
+  // access the neighbor node
+  // at each node, we want to see obj.value and apply the cb
+
+
 };
 
 /*
