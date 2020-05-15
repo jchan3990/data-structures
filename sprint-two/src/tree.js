e9033cddecdf7f478299d942f5f0cea34511a0d2
jchan3990 <jonchan3990@gmail.com>
2020-05-15 00:03:21 -0700

    Finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -15,18 +15,35 @@ var Tree = function(value) {
 
 var treeMethods = {};
 
-
-
 treeMethods.addChild = function(value) {
   // every node is a potential tree
   // create variable to hold new node (or a potential tree)
-  var subtree = Tree(value);
+  var subTree = Tree(value);
   // point to parent node by adding to it's children array (this)
-  this.children.push(subtree);
+  this.children.push(subTree);
 
 };
 
 treeMethods.contains = function(target) {
+
+  var result = false;
+
+  // find target value in node object
+  var findTarget = function(childArray) {
+    // Search the children = array of objects
+    for (var i = 0; i < childArray.length; i++) {
+      // Base case = check if target is equal to node value
+      var currentNode = childArray[i];
+      if (currentNode.value === target) {
+        result = true;
+      } else {
+        // recursive case: search children of current node obj
+        findTarget(currentNode.children);
+      }
+    }
+  }
+  findTarget(this.children);
+  return result;
 };
 
 
