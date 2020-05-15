99fa6b87357f50269bafeafb527311f1e39a050a
jchan3990 <jonchan3990@gmail.com>
2020-05-14 17:49:34 -0700

    Finsihed addChild for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,14 +3,27 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  // .children property, an array containing a number of subtrees
+  // change to an empty array to contain subtrees
+  newTree.children = [];  // fix me
+
+  // extend from sources to destination (tree methods to new tree)
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 var treeMethods = {};
 
+
+
 treeMethods.addChild = function(value) {
+  // every node is a potential tree
+  // create variable to hold new node (or a potential tree)
+  var subtree = Tree(value);
+  // point to parent node by adding to it's children array (this)
+  this.children.push(subtree);
+
 };
 
 treeMethods.contains = function(target) {
