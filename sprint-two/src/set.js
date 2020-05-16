13342b0e3dc2187ca2cbb7c76ad7a5d5b823999c
jchan3990 <jonchan3990@gmail.com>
2020-05-15 19:46:44 -0700

    Finished Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
+// An .add() method, takes any string and adds it to the set
 setPrototype.add = function(item) {
+  // Add input string as key in object
+  this._storage[item] = true;
 };
 
+// A .contains() method, takes any string and returns a boolean reflecting whether it can be found in the set
 setPrototype.contains = function(item) {
+  // Use .hasOwnProperty to check if item exists in object
+  return this._storage.hasOwnProperty(item);
 };
 
+// A .remove() method, takes any string and removes it from the set, if present
 setPrototype.remove = function(item) {
+  // Do property look up
+  // Remove if defined
+  if (this._storage.hasOwnProperty(item)) {
+    delete this._storage[item];
+  }
 };
 
 /*
